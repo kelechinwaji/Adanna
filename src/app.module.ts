1b7c77f3aa10162a466dce6eafb11baa3325c77c
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-10-22 07:09:07 +0100

    [feat]: database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
+dotenv.config();
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
