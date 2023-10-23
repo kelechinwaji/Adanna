d3dd58ff436101080274179871cfcb05a16bc011
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-10-23 19:31:55 +0100

    [feat]: sign up service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,6 +4,8 @@ import { JwtService } from '@nestjs/jwt';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { hashPassword, comparePassword } from 'src/helper/password';
+import { TYPES } from 'src/enum/type.enum';
 
 @Injectable()
 export class AuthService {
@@ -18,9 +20,48 @@ export class AuthService {
       throw new BadRequestException(
         `User with email ${body.email} already exists`,
       );
+
+    // Hash password
+    const hashedPassword = await hashPassword(body.password);
+
+    // Create user
+    const newUser = new this.User({
+      email: body.email,
+      password: hashedPassword,
+      type: TYPES.TYPEONE,
+    });
+    await newUser.save();
+
+    // Get jwt token
+    const token = this.generateAuthToken(newUser);
+
+    const user = newUser.toObject();
+    delete user.password;
+
+    return { user, token };
   }
 
   async getUserByEmail(email: string) {
     return await this.User.findOne({ email });
+  }
+
+  /**
+   * Generates an authentication token for the given user
+   *
+   * @param user The user for whom the token is generated
+   * @returns The authentication token
+   */
+  private generateAuthToken(user: User): string {
+    const payload = {
+      id: user['_id'],
+      email: user.email,
+      type: user.type,
+    };
+
+    const token = this.jwt.sign(payload, {
+      secret: process.env.JWT_SECRET_KEY,
+    });
+
+    return token;
   }
 }
