6ec5f4aaa7fe94f0c97f8c4e8b77395b328eb5e4
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-10-24 18:06:38 +0100

    port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
